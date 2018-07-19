85d0d102745be0843db0dfe912cf2341a3515a87
David Pattinson <djp65@cam.ac.uk>
2018-07-19 16:10:23 +0100

    Revert "Minor changes."
    This reverts commit e9edc16d47d395beb74f194dcbce80d632b82603.

--- prickle/prickle.py
+++ prickle/prickle.py
@@ -113,13 +113,11 @@ class Prickle(object):
         if not hasattr(self, 'segments'):
             self._make_segments()
 
-        lc = LineCollection(
-            segments=self.segments,
-            linewidths=kwds.pop('linewidths', 1),
-            colors=kwds.pop('colors', 'black'),
-            **kwds)
-
         ax = plt.gca()
+        linewidths = kwds.pop('linewidths', 1)
+        colors = kwds.pop('colors', 'black')
+        lc = LineCollection(
+            segments=self.segments, linewidths=linewidths, colors=colors, **kwds)
         ax.add_artist(lc)
         return ax
 
@@ -136,7 +134,6 @@ class Prickle(object):
         """
         self.plot_dots(**dot_kwds)
         self.plot_prickles(**prickle_kwds)
-
         ax = plt.gca()
         ax.set_xticks(self.cols)
         ax.set_yticks(self.rows)
